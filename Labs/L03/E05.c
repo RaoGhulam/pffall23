452a7d6f12c2fc490e375daa20a0c722aa038053
RaoGhulam <142868044+RaoGhulam@users.noreply.github.com>
2023-09-10 00:42:04 +0500

    Add files via upload

---
+++ Labs/L03/E05.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main() {
+             int total_distance = 0;
+             int fuel_consumed = 0;
+
+             total_distance = 10 + 10 + 5 + 15;
+             fuel_consumed = 2*total_distance;
+             printf("Total distance: %d", total_distance);
+             printf("\nTotal fuel consumed: %d", fuel_consumed); 
+}
