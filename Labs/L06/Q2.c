f4deaeec36b1e4a65048034d476f7615b40724d9
RaoGhulam <142868044+RaoGhulam@users.noreply.github.com>
2023-10-03 09:54:18 +0500

    Add files via upload

---
+++ Labs/L06/Q2.c
@@ -0,0 +1,28 @@
+/*
+Programmer: Rao Ghulam Mohi Uddin
+Dec: Lab-6, Q1
+Date: 3/10/2023
+*/
+
+# include <stdio.h>
+
+int main(){
+	int a=0;
+	int b=1;
+	int temp=0;
+	int sum=0;
+	
+	while(a<10000){
+		temp=b;
+		b=b+a;
+		a=temp;
+		if(a<=10000){
+			printf("%d\n",a);
+					if ((a%3==0) || (a%5==0) || (a%7 ==0)){
+						sum = sum + a;
+					}
+		}
+	}
+	printf("Sum = %d",sum);
+	return 0;
+}
