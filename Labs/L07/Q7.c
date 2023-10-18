57d0de3c2489daf581249462e428189935a0a8c3
RaoGhulam <142868044+RaoGhulam@users.noreply.github.com>
2023-10-18 22:52:13 +0500

    Add files via upload

---
+++ Labs/L07/Q7.c
@@ -0,0 +1,46 @@
+/*
+Programmer: Rao Ghulam Mohi Uddin
+Description: Matrix Multiplication
+Date: 17/10/2023
+*/
+
+#include <stdio.h>
+
+int main() {
+    int matrix1[2][2];
+    int matrix2[2][2];
+    int result[2][2];
+
+    printf("Enter elements of the first 2x2 matrix:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("Enter elements of the second 2x2 matrix:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < 2; k++) {
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+
+    printf("Resultant matrix after multiplication:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
